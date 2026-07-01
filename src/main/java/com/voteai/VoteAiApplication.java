951656300de6c32c5a6eb8dbcdfa6e62e150a651
anju mahato <anjumahato@Mac.netis.cc>
2026-07-01 12:36:07 +0545

    Initial Spring Boot project

---
+++ src/main/java/com/voteai/VoteAiApplication.java
@@ -0,0 +1,13 @@
+package com.voteai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoteAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/voteai/VoteAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voteai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
